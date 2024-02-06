2298a2c4c9f2073f9fe71710c0e205c58f384f5c
skeral <96560313+skeral@users.noreply.github.com>
2024-02-06 16:10:11 +0900

    Update num_workers for Windows

--- run.py
+++ run.py
@@ -17,14 +17,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--epochs", type=int, default=10)
     parser.add_argument("--batch_size", type=int, default=16)
-    parser.add_argument("--num_workers", type=int, default=4) # if your os is Windows, then set 0
+    parser.add_argument("--num_workers", type=int, default=0) # if your os is Windows, then set 0
     parser.add_argument("--lr", type=float, default=1e-4)
     parser.add_argument("--seed", type=int, default=0)
     parser.add_argument("--data_path", type=str, default="./data")
     parser.add_argument("--save_dir", type=str, default="./log")
     parser.add_argument("--exp_name", type=str, default=None)
-    parser.add_argument("--mode", type=str, default="train")
-    parser.add_argument("--model_name", type=str, default="CustomModel")
+    # model_name
+    # dataset_name
 
     args = parser.parse_args()
     
